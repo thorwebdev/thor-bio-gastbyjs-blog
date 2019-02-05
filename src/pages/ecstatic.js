995214695e08a945a091f91ed4a56b7cae3dc672
Thorsten Schaeff <thorsten@stripe.com>
2019-02-05 00:22:49 +0000

    fix window issue

--- src/pages/ecstatic.js
+++ src/pages/ecstatic.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { Component } from "react";
 import { withPrefix } from "gatsby";
 
-const Ecstatic = () => {
-  window.location.replace(withPrefix("/ecstatic-commerce.pdf"));
-  return <span>Redirecting...</span>;
-};
+class Ecstatic extends Component {
+  componentDidMount() {
+    window.location.replace(withPrefix("/ecstatic-commerce.pdf"));
+  }
+  render() {
+    return <span>Redirecting...</span>;
+  }
+}
 
 export default Ecstatic;
