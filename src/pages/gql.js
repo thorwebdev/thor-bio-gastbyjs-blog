914b322c1607eae66dbfbf507caf0a9a0b5e4f33
Thorsten Schaeff <thorsten@stripe.com>
2019-03-07 09:14:13 +0800

    Add GraphQL slides.

---
+++ src/pages/gql.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Gql extends Component {
+  componentDidMount() {
+    window.location.replace(
+      `https://drive.google.com/file/d/1NPj9876dOfXDC1uh31g3RDcnUqGJbvvv/view?usp=sharing`
+    );
+  }
+  render() {
+    return <span>Redirecting...</span>;
+  }
+}
+
+export default Gql;
