251fc0bc14effcad80fd579c759e8199a6c6838a
Thorsten Schaeff <tschaeff@users.noreply.github.com>
2019-09-17 17:47:47 +0800

    Moving to Singapore (#6)
    * SG blog start.
    
    * Home page update.
    
    * npm updates
    
    * Sharp plugin updates.
    
    * Update Twitter card.
    
    * Update Twitter card once more.
    
    * Update Twitter card img url.
    
    * Update date.
    
    * Additions
    
    * Change date once more.

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -6,7 +6,7 @@ const SEO = () => {
   const title = config.siteTitle;
   const description = config.siteDescription;
   const realPrefix = config.pathPrefix === "/" ? "" : config.pathPrefix;
-  const image = config.siteUrl + realPrefix + config.siteLogo + `?new`;
+  const image = config.siteUrl + realPrefix + config.siteLogo + `?singapore`;
   const blogURL = config.siteUrl + config.pathPrefix;
   const schemaOrgJSONLD = [
     {
