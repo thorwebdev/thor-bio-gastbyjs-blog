4d3ccd1ba0406f8cea77163a004ea3180ad70958
Thorsten Schaeff <thorsten@stripe.com>
2019-09-30 14:15:02 +0800

    Add GraphQL Hong Kong slides.

---
+++ src/pages/gqlHK.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class GqlHK extends Component {
+  componentDidMount() {
+    window.location.replace(
+      `https://drive.google.com/file/d/1rnb-FRDeROrFgfKbECsSyQtYwKHfpxnQ/view?usp=sharing`
+    );
+  }
+  render() {
+    return <span>Redirecting...</span>;
+  }
+}
+
+export default GqlHK;
