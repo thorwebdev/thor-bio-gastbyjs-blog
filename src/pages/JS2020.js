7f8963a1cb3b32e39a94ff3a4fee3053deea9db3
Thor <thorsten-stripe@users.noreply.github.com>
2020-01-11 17:13:38 +0800

    ny2020-changes (#8)

---
+++ src/pages/JS2020.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Gql extends Component {
+  componentDidMount() {
+    window.location.replace(
+      `https://docs.google.com/presentation/d/e/2PACX-1vT2Em7JDKxwBdTFRVWrq5_TZWnxlpFIIfX-nFGk86NBHGizu-ZEHOBqmBEXfVsO-J-d87jX9gT1lM-L/pub?start=false&loop=false&delayms=3000`
+    );
+  }
+  render() {
+    return <span>Redirecting...</span>;
+  }
+}
+
+export default Gql;
