6e00213afbd0e0d9e6c15e713303ae37b2fbc1ca
Thorsten Schaeff <thorsten@stripe.com>
2020-01-24 20:30:56 +0900

    Add Tokyo Meetup Slides.

---
+++ src/pages/tokyo2020.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Gql extends Component {
+  componentDidMount() {
+    window.location.replace(
+      `https://docs.google.com/presentation/d/e/2PACX-1vTSenfjR7J6kmS7x-Lm74dDd2paU_OE5u6_W9vyA4n32o5_MCtATgBsn8vI66KBLR3KIrThFmNusz1J/pub?start=false&loop=false&delayms=3000`
+    );
+  }
+  render() {
+    return <span>Redirecting...</span>;
+  }
+}
+
+export default Gql;
