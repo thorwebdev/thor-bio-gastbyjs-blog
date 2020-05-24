df3e9d0b7aec0bbf1cb8d5dd43472122c9687a5a
Thorsten Schaeff <5748289+thorwebdev@users.noreply.github.com>
2020-05-24 21:58:54 +0800

    Add ThorSticker (#10)
    * Add ThorSticker
    
    * Add .nvmrc
    
    Co-authored-by: Thorsten Schaeff <thorsten@stripe.com>

---
+++ src/components/ThorHammer.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import styled from "styled-components";
+
+const Hammer = styled.a`
+  cursor: pointer;
+  display: block;
+  width: 228px;
+  height: 242px;
+  top: 1em;
+  margin-left: 1em;
+  background-image: url("https://thorsticker.store/static/media/logo.6bfe1b4e.svg");
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center center;
+  position: fixed;
+  z-index: 51;
+  @media (max-width: 900px) {
+    width: 114px;
+    height: 121px;
+    right: 1em;
+  }
+`;
+
+const ThorHammer = () => {
+  return (
+    <Hammer
+      href="https://thorsticker.store"
+      target="_blank"
+      rel="noopener noreferrer"
+    />
+  );
+};
+
+export default ThorHammer;
--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -17,7 +17,7 @@ export const BlogPostTemplate = ({
   description,
   tags,
   title,
-  helmet
+  helmet,
 }) => {
   const PostContent = contentComponent || Content;
 
@@ -62,7 +62,7 @@ BlogPostTemplate.propTypes = {
   contentComponent: PropTypes.func,
   description: PropTypes.string,
   title: PropTypes.string,
-  helmet: PropTypes.object
+  helmet: PropTypes.object,
 };
 
 const ContentBackground = styled.div`
@@ -102,8 +102,8 @@ const BlogPost = ({ data }) => {
 
 BlogPost.propTypes = {
   data: PropTypes.shape({
-    markdownRemark: PropTypes.object
-  })
+    markdownRemark: PropTypes.object,
+  }),
 };
 
 export default BlogPost;
