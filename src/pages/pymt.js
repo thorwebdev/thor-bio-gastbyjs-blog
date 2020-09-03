095aa8f339071dae7e0a518508fb8131c9e6842a
Thorsten Schaeff <thorsten.schaeff@gmail.com>
2020-09-03 18:37:04 +0800

    Add pymt.online go link.

---
+++ src/pages/pymt.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Wad extends Component {
+  componentDidMount() {
+    window.location.replace(
+      `https://www.notion.so/e31ebc43ffe14e18acc84dced5b9d9f2?v=34dafe7bd6754ad5986253c8a3312dfc`
+    );
+  }
+  render() {
+    return <span>Redirecting...</span>;
+  }
+}
+
+export default Wad;
